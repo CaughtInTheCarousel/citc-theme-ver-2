a93101f10ccd45fc4a6c2f8d4f0654cf6a5db1f3
Brett Widmann <bjwidmann@wausaucoated.com>
2014-03-08 17:34:34 -0600

    removed ad slot in single posts

--- reporter/single.php
+++ reporter/single.php
@@ -82,28 +82,6 @@
 
 					<?php engine_link_pages('before=<div class="page-links pagination">&after=</div>'); ?>
 
-					<p class="adslot_2">
-
-						<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-
-							<!-- CITC2 -->
-
-							<ins class="adsbygoogle"
-
-     							style="display:inline-block;width:728px;height:90px"
-
-     							data-ad-client="ca-pub-7853298522005938"
-
-     							data-ad-slot="2898062954"></ins>
-
-						<script>
-
-							(adsbygoogle = window.adsbygoogle || []).push({});
-
-						</script>
-
-					</p>
-
 				</div>
 
 					
